3ca60001dd32448b35daa4cfdb251f5ed553fa1f
CJackYang <ijackyang@sina.cn>
2017-08-20 22:46:48 -0400

    tmp

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,13 +1,29 @@
 let ipc = require('electron').ipcRenderer
 
-let submitClick = (ip) => {
-    // console.log(ip.getElementById('textIp'))
-    let isSuccess = ipc.sendSync('setIp', ip)
-    if (isSuccess) {
-        document.getElementById('content').innerHTML = '<body id="content"><h1>success</h1><button onclick="startTest()">开始测试</button></body>'
-    }
+let submitClick = (stationip, cloudip) => {
+  console.log(stationip, 　cloudip)
+  let isSuccess = ipc.sendSync('setIp', stationip, cloudip)
+  if (isSuccess) {
+    document.getElementById('content').innerHTML = '<body id="content"><h1>success</h1><button onclick="startTest()">开始测试</button><div id="abc"></div></body>'
+  }
 }
 
 let startTest = () => {
-    ipc.send('startTest')
+  ipc.send('startTest')
+  document.getElementById('content').innerHTML = '<body id="content"><div id="abc"></div></body>'
+  var obj = new WxLogin({
+    id: "abc",
+    appid: "wxd7e08af781bea6a2",
+    scope: "snsapi_login",
+    redirect_uri: 'http%3A%2F%2Fwxlogin.siyouqun.com'
+  });
+}
+
+ipc.on('getWechatCode', () => {
+
+})
+
+function getCode() {
+
+  ipc.emit('getWechatCode', '')
 }
--- src/main/dispatch.js
+++ src/main/dispatch.js
@@ -1,9 +1,10 @@
 const { ipcMain } = require('electron')
 let chalk = require('chalk')
 
-ipcMain.on('setIp',　(event, arg) => {
-    global.stationIP = arg
-    console.log(chalk.magenta('set station ip success : ' + arg))
+ipcMain.on('setIp',　(event, stationIP, cloudIP) => {
+    global.stationIP = stationIP
+    global.cloudIP = cloudIP
+    console.log(chalk.magenta('set station ip success : ' + stationIP), chalk.green('Cloud IP: ' + cloudIP))
     event.returnValue = true
 })
 
